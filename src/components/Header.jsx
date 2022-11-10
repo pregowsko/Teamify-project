f6910e0d7cfc328397dcbe90f224a8e8ef27cae3
Ola <ola.pregowska975@gmail.com>
2022-11-10 17:51:11 +0100

    add Header scss and variables

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {BrowserRouter, Link } from 'react-router-dom';
 
+import '../styles/Header.scss';
+
 const Header = () => {
     return (
         <BrowserRouter>
@@ -11,32 +13,32 @@ const Header = () => {
                     </Link>
                     <nav className="header_menu">
                         <ul className="header_menu_list">
-                            <li className="header__menu__listItem">
+                            <li className="header_menu_listItem">
                                 <Link to="/freelancers">Freelancers</Link>
                             </li>
-                            <li className="header__menu__listItem">
+                            <li className="header_menu_listItem">
                                 <Link to="/aboutus">About Us</Link>
                             </li>
-                            <li className="header__menu__listItem">
+                            <li className="header_menu_listItem">
                                 <Link to="/casestudy">Case Study</Link>
                             </li>
-                            <li className="header__menu__listItem">
+                            <li className="header_menu_listItem">
                                 <Link to="/reviews">Rewievs</Link>
                             </li>
-                            <li className="header__menu__listItem">
+                            <li className="header_menu_listItem">
                                 <Link to="/newsletter">Newsletter</Link>
                             </li>
                         </ul>
                     </nav>
-                    <hr />
-                    <div className="header_title">
-                        <p>Meet your future team!</p>
-                    </div> 
-                    <form action="" className="header_form">
-                        <input type="text" className="header_form_input"/>
-                        <button className="header_form_button">Search</button>
-                    </form>
                 </div>
+                <hr className='header_line'/>
+                <div className="header_title">
+                    <p>Meet your future team!</p>
+                </div> 
+                <form action="" className="header_form">
+                    <input type="text" className="header_form_input" placeholder="Search by Nickname"/>
+                    <button className="header_form_button">Search</button>
+                </form>
             </header>
         </BrowserRouter>
     );
